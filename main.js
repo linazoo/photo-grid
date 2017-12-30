2a1050c50f218d3eee1b436e82212c404e46a8e0
lina zoo <linazooper@gmail.com>
2017-12-30 15:20:44 -0600

    planning grid for photos

---
+++ main.js
@@ -0,0 +1,18 @@
+(function() {
+  // make instagram GET request 
+
+  // loop through array of images
+
+  // create HTML for each image item
+
+  /**
+  set a background image on this div and apply the right size class
+  
+  <div class="item__details">
+    jelly-o brownie sweet
+  </div>
+   
+   */
+
+  // append to grid Div
+})();
