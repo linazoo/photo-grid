e1bcac1e454555364bd79f47aa83a7fb262329e5
lina zoo <linazooper@gmail.com>
2017-12-30 16:22:38 -0600

    programatically pulling images into a grid template. displaying sorta properly right now

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 (function () {
   // make instagram GET request 
 
-
   // loop through array of images
 
   // create HTML for each image item
@@ -13,40 +12,41 @@
     jelly-o brownie sweet
   </div>
   */
-  var token = '7060436.be13e05.6691ed79dd50421f95a09dc182c3fb10',
-  username = 'lilemoji', // rudrastyh - my username :)
-  num_photos = 100;
 
-$.ajax({ // the first ajax request returns the ID of user rudrastyh
-url: 'https://api.instagram.com/v1/users/search',
-dataType: 'jsonp',
-type: 'GET',
-data: {access_token: token, q: username}, // actually it is just the search by username
-success: function(data){
-  console.log(data);
-  $.ajax({
-    url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent', // specify the ID of the first found user
+  var token = '7060436.be13e05.6691ed79dd50421f95a09dc182c3fb10',
+    username = 'lilemoji', // rudrastyh - my username :)
+    num_photos = 20;
+
+  $.ajax({ // the first ajax request returns the ID of user rudrastyh
+    url: 'https://api.instagram.com/v1/users/search',
     dataType: 'jsonp',
     type: 'GET',
-    data: {access_token: token, count: num_photos},
-    success: function(data2){
-      console.log(data2);
-      for(x in data2.data){
-        $('ul').append('<li><img src="'+data2.data[x].images.thumbnail.url+'"></li>');  
-      }
+    data: { access_token: token, q: username }, // actually it is just the search by username
+    success: function (data) {
+      console.log(data);
+      $.ajax({
+        url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent', // specify the ID of the first found user
+        dataType: 'jsonp',
+        type: 'GET',
+        data: { access_token: token, count: num_photos },
+        success: function (data2) {
+          console.log(data2);
+          for (x in data2.data) {
+            var item = document.createElement('div');
+            item.classList.add('item');
+            $(item).css('background-image', `url(${data2.data[x].images.thumbnail.url})`);
+            // itemHtml += '</div>'
+            $('.grid').append(item);
+          }
         },
-    error: function(data2){
-      console.log(data2);
+        error: function (data2) {
+          console.log(data2);
+        }
+      });
+    },
+    error: function (data) {
+      console.log(data);
     }
   });
-},
-error: function(data){
-  console.log(data);
-}
-});
-
-
-  // append to grid Div
-
 
 })();
